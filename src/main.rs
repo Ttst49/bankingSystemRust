4d2d081bea37275d3106d62190c104dabde47890
Thibaut <thibautstachnick@gmail.com>
2024-06-15 11:33:19 +0200

    add a user system in rust

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,48 @@
+#[derive(Debug)]
+struct User{
+    id:i32,
+    first_name:Option<String>,
+    last_name:Option<String>,
+    username:String,
+    password:String
+}
+
+impl User {
+    fn new(
+           username:String,
+           password:String,
+           first_name:Option<String>,
+           last_name:Option<String>
+    )->User{
+        let mut user = User{
+            id: 0,
+            first_name: None,
+            last_name: None,
+            username,
+            password
+        };
+        if let Some(payload) = first_name{
+            user.first_name = Some(payload.to_string())
+        }else {
+            user.first_name = Some("undefined".to_string())
+        }
+        if let Some(payload) = last_name{
+            user.last_name = Some(payload.to_string())
+        }else {
+            user.last_name = Some("undefined".to_string())
+        }
+        user
+    }
+}
+
+fn show_menu(){}
+
 fn main() {
-    println!("Hello, world!");
+    let user = User::new(
+        String::from("tibo"),
+        String::from("password"),
+        Some(String::from("Thibaut")),
+        None
+    );
+    println!("{:?}",user)
 }
